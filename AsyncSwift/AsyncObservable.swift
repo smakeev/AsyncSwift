68092c6c1ad0d1fdb7b93120a8d8d0ea8477e616
Sergey Makeev <makeev.87@gmail.com>
2020-11-25 18:44:04 +0300

    Async Observable implementation

--- AsyncSwift/AsyncObservable.swift
+++ AsyncSwift/AsyncObservable.swift
@@ -7,107 +7,103 @@
 //
 
 import Foundation
-
 public protocol AsyncObserver {
-	associatedtype ResultType
-	associatedtype ErrorType: Error
-	@discardableResult mutating func addObserver(_ observer: AnyObject, on queue: DispatchQueue, _ handler: @escaping (Result<ResultType?, ErrorType>) -> Void) -> Bool
-	@discardableResult mutating func removeObserver(_ observer: AnyObject) -> Bool
+    associatedtype ObservableType
+    @discardableResult mutating func addObserver(_ observer: AnyObject, on queue: DispatchQueue?,  handler: @escaping (ObservableType) -> Void) -> Bool
+    @discardableResult mutating func removeObserver(_ observer: AnyObject) -> Bool
 }
 
 @propertyWrapper
-struct AsyncObservable<T, E: Error>: AsyncObserver {
-	
-	
-	private var queue: DispatchQueue? = nil
-	public init(_ queue: DispatchQueue? = nil) {
-		self.queue = queue
-		self.value = Result.success(nil)
-	}
+struct AsyncObservable<T>: AsyncObserver {
+    public typealias ObservableType = T
 
-	private var value: Result<T?, E>
-	public var wrappedValue: Result<T?, E> {
-		get {
-			return value
-		}
-		
-		set {
-			self.value = newValue
-		
-			self.syncQueue.sync {
-				self.observers = self.observers.filter {
-					$0.observer != nil
-				}
-				for observer in observers {
-					observer.queue.async {
-						observer.handler(newValue)
-					}
-				}
-			}
-		}
-	}
-	
-	public typealias ResultType = T
-	public typealias ErrorType  = E
-	
-	private let syncQueue: DispatchQueue = DispatchQueue(label: "AsyncObserverSyncQueue")
-	
-	private struct Wrapper {
-		var observer: AnyObject? = nil
-		var handler: ((Result<T?,E>) -> Void)!
-		var queue: DispatchQueue
-	}
-	
-	private var observers = [Wrapper]()
-	
-	@discardableResult public mutating func addObserver(_ observer: AnyObject, on queue: DispatchQueue, _ handler: @escaping (Result<T?,E>) -> Void) -> Bool {
-		return syncQueue.sync {
-			guard (observers.filter {
-				if let observer = $0.observer {
-					return observer === observer
-				} else {
-					return false
-				}
-			}).count == 0 else {
-				print("AsyncSwift WARNING: observer already exists. Remove it first to change handler")
-				return false
-			}
-			self.observers.append(Wrapper(observer: observer, handler: handler, queue: queue))
-			return true
-		}
-	}
-		
-	@discardableResult public mutating func removeObserver(_ observer: AnyObject) -> Bool {
-			return syncQueue.sync {
-				var result = false
-				self.observers = observers.filter {
-					if let validObserver = $0.observer {
-						if validObserver === observer {
-							result = true
-							return false
-						}
-					} else {
-						result = true
-						return false
-					}
-					return true
-				}
-				return result
-			}
-		}
-	
-	public var projectedValue: T? {
-		get {
-			switch value {
-			case .success(let data):
-				return data
-			case .failure:
-				return nil
-			}
-		}
-		
-		set {
-			value = .success(newValue)
-		}
-	}
+
+    public var wrappedValue: T {
+        get {
+            return value
+        }
+        
+        set {
+            self.value = newValue
+            self.syncQueue.sync {
+                let elementsToBeDeleted = NSMutableArray()
+                for element in observers {
+                    let element = element as! Wrapper
+                    if element.observer == nil {
+                        elementsToBeDeleted.add(element)
+                    } else {
+                        element.handler(newValue)
+                    }
+                }
+
+                for element in elementsToBeDeleted {
+                    observers.remove(element)
+                }
+
+            }
+        }
+    }
+    
+    private var queue: DispatchQueue? = nil
+    private var value: T
+    private let syncQueue: DispatchQueue = DispatchQueue(label: "AsyncObserverSyncQueue")
+    
+    private class Wrapper: NSObject {
+        weak var observer: AnyObject? = nil
+        var handler: (T) -> Void
+        var queue: DispatchQueue
+        
+        init(observer: AnyObject, handler: @escaping (T) -> Void, queue: DispatchQueue) {
+            self.observer = observer
+            self.handler  = handler
+            self.queue    = queue
+        }
+    }
+    
+    private var observers = NSMutableArray() //[Wrapper]()
+    
+    public init(wrappedValue value: T, _ queue: DispatchQueue? = nil) {
+        self.queue = queue
+        self.value = value
+    }
+    
+    @discardableResult public func addObserver(_ observer: AnyObject, on queue: DispatchQueue? = nil,  handler: @escaping (T) -> Void) -> Bool {
+        return syncQueue.sync {
+            guard (observers.filter {
+                guard let element = $0 as? Wrapper else { return false }
+                if let observer = element.observer {
+                    return observer === observer
+                } else {
+                    return false
+                }
+            }).count == 0 else {
+                print("AsyncSwift WARNING: observer already exists. Remove it first to change handler")
+                return false
+            }
+            self.observers.add(Wrapper(observer: observer, handler: handler, queue: queue ?? DispatchQueue.main))
+            return true
+        }
+    }
+        
+    @discardableResult public func removeObserver(_ observer: AnyObject) -> Bool {
+            return syncQueue.sync {
+                var elementToBeRemoved: Wrapper?
+                for element in observers {
+                    if let element = element as? Wrapper {
+                        if element.observer === observer {
+                            elementToBeRemoved = element
+                        }
+                    }
+                }
+                if let element = elementToBeRemoved {
+                    observers.remove(element)
+                    return true
+                }
+                return false
+            }
+        }
+    
+    public var projectedValue:  AsyncObservable<T> {
+            return self
+    }
 }
--- AsyncSwiftTests/AsyncObservablesTests.swift
+++ AsyncSwiftTests/AsyncObservablesTests.swift
@@ -9,19 +9,37 @@
 import XCTest
 
 class AsyncObservablesTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-	@AsyncObservable(DispatchQueue.main) var value: Result<Int?, Error>
-    func testCreation() {
-		value = .success(17)
-		print("!!! \($value)")
+	
+	override func setUp() {
+		// Put setup code here. This method is called before the invocation of each test method in the class.
 	}
-
+	
+	override func tearDown() {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+	}
+	
+	@AsyncObservable(DispatchQueue.main) var value = 8
+	func testCreation() {
+		XCTAssert(value == 8)
+		value = 17
+		XCTAssert(value == 17)
+	}
+	
+	func testObservers(){
+		var observerCalled = false
+		$value.addObserver(self) { newValue in
+			XCTAssert(newValue == 5)
+			observerCalled = true
+		}
+		XCTAssert(observerCalled == false)
+		value = 5
+		XCTAssert(value == 5)
+		XCTAssert(observerCalled == true)
+		observerCalled = false
+		$value.removeObserver(self)
+		value = 6
+		XCTAssert(value == 6)
+		XCTAssert(observerCalled == false)
+	}
+	
 }
